5bdb34981d9f86d7ac11f572614bb244b0cf93a7
Tawhidur Noor <56801563+tawhidurnoor@users.noreply.github.com>
2021-09-16 02:03:12 +0600

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/pages/choose_location.dart';
+//import 'pages/home.dart';
+import 'package:world_time/pages/home.dart'; //this line is same ase abobe's but a good practise
+import 'package:world_time/pages/loading.dart';
+
+void main() => runApp(MaterialApp(
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => const Loading(),
+        '/home': (context) => const Home(),
+        '/location': (context) => const ChooseLocation(),
+      },
+    ));
